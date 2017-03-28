99d2c437bc8cd14378ad623faf4917ee934d97fb
pclaquin <pierreclaq@gmail.com>
2017-03-28 09:45:27 +0200

    Update test.java

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("I made a pie !");
     }   
 }
