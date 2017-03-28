de354d888fa1c7392ffd50811252f339c945fcd2
pclaquin <pierreclaq@gmail.com>
2017-03-28 09:38:14 +0200

    Update test.java

--- test.java
+++ test.java
@@ -2,4 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.printl
+        System.out.println("Hello, World");
+    }
+
+}
