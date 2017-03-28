29c7b16bf1d79c7486fd8a9945d729b04fa7443c
pclaquin <pierreclaq@gmail.com>
2017-03-28 09:34:17 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,15 +1,7 @@
-import java.awt.Dimension;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.awt.Toolkit;
+public class HelloWorld {
 
-public void takeScreenshot(String file) throws Exception {
-    Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-    Rectangle rect = new Rectangle(size);
-    Robot robot = new Robot();
-    BufferedImage image = robot.createScreenCapture(rect);
-    ImageIO.write(image, "png", new File(fileName));
-}
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
